3262b0b2bcf786654e3bf809f8494010bff1f911
Prateek-47 <shrmapratik11@gmail.com>
2025-07-03 11:32:27 +0530

    version2 commit

---
+++ lib/app/data/models/case_info_model.dart
@@ -0,0 +1,13 @@
+// lib/app/data/models/case_info_model.dart
+
+class CaseInfo {
+  final String id;
+  final String title;
+  final String description;
+
+  CaseInfo({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
